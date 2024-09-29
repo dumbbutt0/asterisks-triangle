9e105656529b3d739f002e6c2b43eab1738bf8a0
dumbbutt0 <134574598+dumbbutt0@users.noreply.github.com>
2024-09-29 02:09:24 -0700

    optimized blackjack.cpp

--- blackjack.cpp
+++ blackjack.cpp
@@ -3,179 +3,100 @@
 
 using namespace std;
 
-
 random_device deck;
 mt19937 cards(deck());
 uniform_int_distribution<> shuffle(1, 11);
 uniform_int_distribution<> shuffled(2, 21);
 uniform_int_distribution<> play(1, 2);
-int user,dealer, card1=shuffle(cards),card2=shuffle(cards),card3=shuffled(cards),go=play(cards);
-void pausing(),question();
+int user,dealer,c1,c2,c3;
+void pausing(),deal(),rounder();
 
 void game()
 {
+    char answer;
+    c1=shuffle(cards);
+    c2=shuffle(cards);
+    c3=shuffled(cards);
 
-   
-    string round="> Do you want another card? (y/n): ",again="> Would you like to play again? (y/n): ";
-    char yes='y',no='n',answer;
-    cout << endl;
-    
-    user=0;
-    dealer=0;
-
-    dealer = card3;
-    cout << "> The Dealer starts with a " << dealer<<endl;
-    cout<< "> Your first cards: "<< card1 << ","<<card2<<endl;
-    user=card1+card2;
-    cout << "> Total:"<< user<< endl;
-    if (user >21){
-        cout << "> Dealer wins!\n";
+    dealer = c3;
+    cout << "> The Dealer starts with a " << dealer<< endl;
+    cout<< "> Your first cards: "<< c1 << ","<<c2<<endl;
+    user =c1+c2;
+    cout << "> Total: "<< user<< endl;
+    if (user == 21){
+        cout << "> Player wins by 21!"<<endl;
         return;
-    }
+        }
     cout << "> hit? (y/n): ";
     cin >> answer;
-    cout << endl;
-    if (answer == yes)
-    {
-        card1=shuffle(cards);
-        user+=card1;
-        cout << "> Card: "<< card1 << endl;
-        cout << "> Total: " <<user << endl;
-        if (user >21){
-            cout << "> Dealer wins!\n";
-            return;
-        }
-        cout << "> hit? (y/n): ";
-        cin >> answer;
-        cout << endl;
-        if (answer == yes)
-        {
-            card1=shuffle(cards);
-            user+=card1;
-            cout << "> Card: "<< card1 << endl;
-            cout << "> Total: " <<user << endl;
-            if (user >21){
-            cout << "> Dealer wins!\n";
-            return;
-            }
-            cout << "> hit? (y/n): ";
-            cin >> answer;
-            cout << endl;
-            if (user >21){
-            cout << "> Dealer wins!\n";
-            return;
-            }
-            if (answer == yes)
-            {
-                card1=shuffle(cards);
-                user+=card1;
-                cout << "> Card: "<< card1 << endl;
-                cout << "> Total: " <<user << endl;
-                if (user >21){
-                cout << "> Dealer wins!\n";
-                return;
-                }
-                cout << "> hit? (y/n): ";
-                cin >> answer;
-                cout << endl;
-                if (answer == yes)
-                {
-                    card1=shuffle(cards);
-                    user+=card1;
-                    cout << "> Card: "<< card1 << endl;
-                    cout << "> Total: " <<user << endl;
-                    if (user >21){
-                    cout << "> Dealer wins!\n";
-                    return;
-                    }
-                }
-            }
-        }
+    if (answer == 'y'){
+        rounder();
+    }else{
+        pausing();
     }
-    pausing();
 }
 
 
-void deals()
+void rounder()
 {
-    
-    if (dealer > 21){
-            cout << "> Dealer busted with "<< dealer;
-            return;
-        }
-
-    go=play(cards);
-    if (dealer >=17){
-        cout << "> Dealer has "<< dealer << endl;
-        if (go == 1|| dealer){
-                card3=shuffle(cards);
-                dealer += card3;
-                cout <<"> Dealer gets a "<< card3<< endl;
-                cout << "> Total: "<<dealer<<endl;
-                if (dealer > 21){
-                    cout << "> Dealer busted with "<< dealer<< endl;
-                    return;
-                    }   
-            }else{
-                dealer +=card3;
-                pausing();
-            }
-            if ( go == 2){
-                int x = 21- user;
-                int y = 21 - dealer;
-                cout << "> The Dealer holds\n";
-                    if (x <y){
-                        cout << "> YOU BEAT THE DEALER!!\n";
-                    }else{
-                        cout <<"> The dealer wins.\n";
-                    }
-                return;
-            }
-    
-    } else{
-        card3=shuffle(cards);
-        dealer += card3;
-        cout <<"> Dealer gets a "<< card3<< endl;
-        cout << "> Total: "<<dealer<<endl;
-    }
-    if (go == 2){
-        
-        if (dealer >21){
-            cout << "> YOU BEAT THE DEALER!!"<< endl;
-            question();
-        }
-       int x = 21- user;
-       int y = 21 - dealer;
-       cout << "> The Dealer holds\n";
-        if (x <y){
-            cout << "> YOU BEAT THE DEALER!!"<< endl;
-        }else if (x == y)
-        {
-            cout << "> There was a draw\n";/* code */
-        }
-        {
-            cout <<"> The dealer wins.\n"<<endl;
-        }
+    char answer;
+    c1=shuffle(cards);
+    user += c1;
+    cout << "> Card: "<< c1 << endl;
+    cout << "> Total: " <<user << endl;
+    if (user >21)
+    {
+        cout << "> Dealer wins!\n";
         return;
+    } 
+    cout << "> hit? (y/n): ";
+    cin >> answer;
+    if (answer == 'y'){
+        rounder();
+    }else{
+        pausing();
     }
-    pausing();
 }
 
 
 void pausing()
 {
     char a;
-    a='a';
     cout << "> (c to continue)";
     cin >> a;
     cout << endl;
-    deals();
-
+    //deal();
     if (a != 'c'){
         pausing();
     }
+    deal();
 }
 
+
+void deal()
+{
+    int go=play(cards);
+    if (dealer < 17){
+        c3=shuffle(cards);
+        dealer += c3;
+        cout <<"> Dealer gets a "<< c3<< endl;
+        cout << "> Total: "<<dealer<<endl;
+    }if (dealer > 21){
+        cout << "> Dealer busted with "<< dealer<< endl;
+        return;
+    }if (go ==1){
+        c3=shuffle(cards);
+        dealer += c3;
+        cout <<"> Dealer gets a "<< c3<< endl;
+        cout << "> Total: "<<dealer<<endl;
+    }if (dealer > 21){
+        cout << "> Dealer busted with "<< dealer<< endl;
+        return;
+    }if (dealer ==21){
+        cout << ". Dealer wins by 21!";
+    }
+    pausing();
+}
 
 void question()
 {
@@ -192,8 +113,8 @@ void question()
         cout << "> Goodbye😁️\n";
     }
 }
-int main()
-{
+
+int main(){
     game();
     question();
 }
